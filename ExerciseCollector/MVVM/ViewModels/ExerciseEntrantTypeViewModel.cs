8e621b1a9e901a694f6df0c0849f74077db99caa
Andrey Solodnikov <andrey.solodnikov@saritasa.com>
2024-03-15 00:47:45 +0700

    Update view models

--- ExerciseCollector/MVVM/ViewModels/ExerciseEntrantTypeViewModel.cs
+++ ExerciseCollector/MVVM/ViewModels/ExerciseEntrantTypeViewModel.cs
@@ -5,14 +5,22 @@ namespace ExerciseCollector.MVVM.ViewModels;
 
 public partial class ExerciseEntrantTypeViewModel
 {
+    public ExercisePickerModel ExercisePickerModel { get; } = new();
+
     [RelayCommand]
     public async Task NavigateToGenderPage(ExerciseEntrantType exerciseEntrantType)
     {
+        if (ExercisePickerModel is null)
+        {
+            throw new InvalidOperationException($"{nameof(ExercisePickerModel)} property is null");
+        }
+
+        ExercisePickerModel.ExerciseEntrantType = exerciseEntrantType;
         await Shell.Current.GoToAsync("gender", new Dictionary<string, object>
         {
             {
-                nameof(ExerciseEntrantType),
-                exerciseEntrantType
+                nameof(ExercisePickerModel),
+                ExercisePickerModel
             } 
         });
     }
